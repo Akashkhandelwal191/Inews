5c1f89dd243c45336fb011a6ab911b67d9b9e950
Akash khandelwal <skykhandelwal1999@gmail.com>
2025-03-16 00:39:26 +0530

    Added key for R2 uploader

--- Js/config.js
+++ Js/config.js
@@ -6,3 +6,6 @@ var key2 = "ae1851bbf367b873c2025eb7d6ffbbd0";
 var key3 = "fe7b37dd8d65352cdd43a6e73b18cedf"; 
 // Added key for AWS
 var key4 = "djsajdsadklksaldksakdlakdlad";
+
+//Added key for R2 uploader
+var key5 = "sadsadasdasdasddsdasdadadsd";
